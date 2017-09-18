095d0708065a307872eb487cac47a79828f5c75d
shutom <sh.t.jax.85@gmail.com>
2017-09-18 14:54:23 +0900

    Replace and Make the src files

---
+++ src/server.jsx
@@ -0,0 +1,25 @@
+import express from 'express';
+import path from 'path';
+import React from './modules/render.jsx';
+
+var app = express();
+
+app.use(express.static(path.join(__dirname, '..', 'build')));
+
+// for server side logic
+app.get('api/items', function (req, res, next) {
+  res.json([
+    {id: 1, text: 'first'},
+    {id: 2, text: 'second'},
+    {id: 3, text: 'third'}
+  ]);
+});
+
+// for server side rendering
+app.get('*', function (req, res, next) {
+  React(req, res);
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
